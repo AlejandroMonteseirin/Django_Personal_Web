b5ff9820a30234bacbefdf1d32d3bee12d73f354
AlejandroMonteseirin <alexmonteseirin@gmail.com>
2019-11-05 21:36:16 +0100

    primer modelo y sistema de stalkeo

--- PaginaPersonalAlexV1/PaginaPersonal/views.py
+++ PaginaPersonalAlexV1/PaginaPersonal/views.py
@@ -3,16 +3,38 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 import requests
-
+from .models import Conexiones
+import datetime
 
 
 def index(request):
-
     #esto es como el controlador/servicios
     ethereum=get_latest_crypto_price('ETH')
     litecoin=get_latest_crypto_price('LTC')
 
     context = {'ethereum': ethereum,'ethereumcoins':0.2470,'lite': litecoin,'litecoins':0.5584,'total':ethereum*0.2470+litecoin*0.5584}
+
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    print(ip)
+
+    datosip=getGeoDatos(ip)
+    print(datosip['type']!=None)
+    if datosip['type']!=None:
+        #fechas
+        today_min = datetime.datetime.combine(datetime.date.today(), datetime.time.min)
+        today_max = datetime.datetime.combine(datetime.date.today(), datetime.time.max)
+        conexion=Conexiones(fecha__range=(today_min, today_max),ip=datosip['ip'])
+        if(len(conexion)>0):
+            conexion(numeroConexiones=conexion['numeroConexiones']+1)
+            conexion.save()
+        else:
+            c = Conexiones(numeroConexiones='1', fecha=datetime.datetime.now(),ip=datosip['ip'],pais=datosip['country_name'],ciudad=datosip['city'],postcode=datosip['zip'],coordenadas=datosip['latitude']+datosip['longitude'])
+            c.save()
+    
     return render(request, 'index.html',context)
 
 
@@ -27,3 +49,9 @@ def get_latest_crypto_price(crypto):
     response = requests.get(url)
     response_json = response.json()
     return float(response_json['data']['amount'])
+
+def getGeoDatos(ip):
+    url ="http://api.ipstack.com/"+ip+"?access_key=5214629bd7a2ce39efe99b8bc64741a2"
+    response = requests.get(url)
+    response_json = response.json()
+    return response_json
