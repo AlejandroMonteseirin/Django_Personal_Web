7e9d530379057aedb8c4eadcdad24a6d908234ab
AlejandroMonteseirin <alexmonteseirin@gmail.com>
2019-11-05 01:15:15 +0100

    Crypto

--- PaginaPersonalAlexV1/PaginaPersonal/views.py
+++ PaginaPersonalAlexV1/PaginaPersonal/views.py
@@ -2,9 +2,28 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+import requests
 
 
 
 def index(request):
+
     #esto es como el controlador/servicios
-    return render(request, 'index.html')
+    ethereum=get_latest_crypto_price('ETH')
+    litecoin=get_latest_crypto_price('LTC')
+
+    context = {'ethereum': ethereum,'ethereumcoins':0.2470,'lite': litecoin,'litecoins':0.5584,'total':ethereum*0.2470+litecoin*0.5584}
+    return render(request, 'index.html',context)
+
+
+
+def llamada(request):
+    #esto es como el controlador/servicios
+    return HttpResponse("return this string")
+
+
+def get_latest_crypto_price(crypto):
+    url = 'https://api.coinbase.com/v2/prices/'+crypto+'-EUR/sell'
+    response = requests.get(url)
+    response_json = response.json()
+    return float(response_json['data']['amount'])
--- PaginaPersonalAlexV1/PaginaPersonalAlexV1/urls.py
+++ PaginaPersonalAlexV1/PaginaPersonalAlexV1/urls.py
@@ -21,5 +21,6 @@ from PaginaPersonal.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('llamada',llamada,name="llamada"),
 
 ]
