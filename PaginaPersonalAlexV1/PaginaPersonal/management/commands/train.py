44a9961376d982f5c170f99704315cf0fc6a5caf
AlejandroMonteseirin <alexmonteseirin@gmail.com>
2019-11-18 17:12:55 +0100

    Route changes

--- PaginaPersonalAlexV1/PaginaPersonal/management/commands/train.py
+++ PaginaPersonalAlexV1/PaginaPersonal/management/commands/train.py
@@ -3,12 +3,13 @@ from django.utils import timezone
 from django.conf import settings
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot import ChatBot
-
+import os
 class Command(BaseCommand):
     help = 'Entrela la IA'
 
     def handle(self, *args, **kwargs):
         chatbot = ChatBot(**settings.CHATTERBOT,storage_adapter="chatterbot.storage.DjangoStorageAdapter")
         trainer = ChatterBotCorpusTrainer(chatbot)
-        trainer.train("../../.././spanish/")
+        print(os.path.dirname(os.path.abspath(__file__)))
+        trainer.train("../../../spanish/")
         self.stdout.write("Entrenadismo")
